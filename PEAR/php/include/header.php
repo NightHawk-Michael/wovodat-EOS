514bb681760e6d7a6547c78098a83ac4c1afed5f
Luis Ngo <baoloc1993@gmail.com>
2017-03-31 15:11:34 +0800

    update PEAR

--- PEAR/php/include/header.php
+++ PEAR/php/include/header.php
@@ -1,91 +1,17 @@
-<?php
-if (!isset($_SESSION))
-    session_start();
-?>
-<!-- Nhat changed, 18 June 2015 -->
-<script src="/js/menu.js"></script>
-<link href="/css/index.css" rel="stylesheet" type="text/css">
-<link href="/css/normalize.css" rel="stylesheet" type="text/css">
-</style>
-<div class="header">
-    <div class="container"> 
-        <div class="header-logo">  <!-- header -->
-            <table>
-                <tr>
-                    <td>
-                        <div class="">
-                            <a href="http://www.wovo.org/">
-                                <img src="/gif2/WOVO_logo.gif" width="50" height="50" />
-                            </a>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="header-name">
-                            <a href="http://www.wovodat.org/">
-                                <span class="header-bigname">WOVOdat</span>
-                                <span class="header-smallname"> A Database of Volcanic Unrest</span></a>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="eos-div">
-                            <a href="http://www.earthobservatory.sg/">
-                                <img src="/gif2/EOS_logo_3.png" />
-                            </a>
-                        </div>
-                    </td>
-                </tr>
-            </table>
-        </div>
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>WOVOdat :: The World Organization of Volcano Observatories (WOVO): Database of Volcanic Unrest (WOVOdat), by IAVCEI</title>
+        <meta http-equiv="content-type" content="text/html;charset=utf-8">
+        <meta http-equiv="content-type" content="text/html;charset=iso-8859-1">
+        <meta name="description" content="The World Organization of Volcano Observatories (WOVO): Database of Volcanic Unrest (WOVOdat)">
+        <meta name="keywords" content="Volcano, Vulcano, Volcanoes">
+        <link href="/css/normalize.css" rel="stylesheet" type="text/css">
+        <link href="/gif2/WOVOfavicon.ico" type="image/x-icon" rel="SHORTCUT ICON">
+        <link href="/css/index.css" rel="stylesheet" type="text/css">
+        <script src="/js/jquery-1.9.1.js"></script>
+        <script src="/js/jssor.slider.mini.js"></script>
+        <script src="/js/index.js"></script>
+		<link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet"> 
 
-        <div class="header-menu">
-                <ul class="left-menu">
-                    <li class="menu-item"><a href="/"><span>Home</span></a></li>
-                    <li>|</li>
-                    <li class="menu-item"><a href="/docum"><span>Documentation</span></a></li>
-                    <li>|</li>
-                    <li class="menu-item"><a><span class='menu-item'>Volcano</span></a></li>
-                    <li>|</li>
-                    <li class="menu-item"><a href="/populate/home_populate.php"><span>SubmitData</span></a></li>
-                    <li>|</li>
-                    <li class="menu-item"><a href="/populate/contact_us_form.php"><span>Contact</span></a></li>
-                </ul>
 
-                <ul class="right-menu" >
-                    <?php
-                    if (isset($_SESSION['login'])) {
-                        ?>
-                        <li>
-                            <a href="/populate/my_account.php"><?php 
-                            $n = $_SESSION['login']['cr_uname'];
-                            $l = strlen($n);
-                            if($l > 12) $n = substr($n,0,12) . '...';
-                            echo $n; 
-                            ?></a>
-                        </li>
-                        <li class="menu-item">
-                            <a href="/populate/logout.php"><span> | Logout</span></a>
-                        </li>
-                        <?php
-                    } else {
-                        ?>
-
-                        <li class="menu-item">
-                            <a href="/populate/index.php"><span>Register | Login</span></a>
-                        </li>
-                        <?php
-                    }
-                    ?>
-                    </ul>
-                <ul class="sub-menu-1">
-                    <li class="menu-item"><div class="stripe"></div><a href="/about"><span>More About WOVOdat</span></a></li>
-                    <li class="menu-item"><a href="/about/useofwovodat.php"><span>Uses of WOVOdat</span></a></li>
-                    <li class="menu-item"><a href="/about/timeline.php"><span>History</span></a></li>
-                </ul>
-                <ul class="sub-menu-2">
-                    <li class="menu-item"><div class="stripe"></div><a href="/boolean/booleanIndex.php"><span>Data Search</span></a></li>
-                    <li class="menu-item"><a href="/precursor/index_unrest_devel_v5.php"><span>Volcano Comparison</span></a></li>
-                    <li class="menu-item"><a href="/eruption"><span>Eruption Interactive</span></a></li>
-                </ul>
-        </div>
-    </div>
-</div>
